8829302ef2ff75a6af4c514d2c831680beedd4bd
rahuljaincse <jenumjaytishashnum>
2014-05-12 19:13:58 +0530

    updated

---
+++ reg1.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Registeration Form</title>
+
+ <img src="rj1.jpg" width="1342px" height="300" alt="@its_Coffee_Time/>
+
+<blink > <h1 align="center" > Register Here  </h1> </blink>
+
+</head>
+
+<marquee onmouseover="this.stop()" onmouseout="this.start()">Jabalpur Engineering Collage Jabalpur </marquee>
+
+
+
+
+<body>
+
+
+<span>
+<table align="center" style bgcolor="#E1B01E">
+<tr>
+<td bgcolor="#FF0000">Student Name</td>
+<td bgcolor="#0080FF"><input type="text"name ="txt1" /></td>
+</tr>
+
+<tr>
+<td bgcolor="#FF0000">Fathers Name</td>
+<td bgcolor="#0080FF"><input type="text"name ="txt2"></td>
+</tr>
+
+<tr>
+<td bgcolor="#FF0000">Mother Name</td>
+<td bgcolor="#0080FF"><input type="text"name ="txt3" /></td>
+</tr>
+
+<tr>
+<td bgcolor="#FF0000">Address</td>
+<td bgcolor="#0080FF"><input type="text"name ="txt4" /></br>
+<input type="text"name ="txt5" /></br>
+<input type="text"name ="txt5" /></td>
+</tr>
+
+
+<tr>
+<td bgcolor="#FF0000">State</td>
+<td bgcolor="#0080FF" bordercolor="#FFFF00"><select>
+<option>Madhya Pradesh<option/><option>Rajasthan<option/><option>Gujrat<option/>
+</select>
+</td>
+</tr>
+
+<tr>
+<td bgcolor="#FF0000"> category </td>
+<td bgcolor="#0080FF"><input type="text"name ="txt8" /></td>
+</tr>
+
+<tr>
+<td bgcolor="#FF0000">Email id</td>
+<td bgcolor="#0080FF" ><input type="text"name ="txt9" /></td>
+</tr>
+
+<tr>
+<td bgcolor="#FF0000">Mobile no.</td>
+<td bgcolor="#0080FF" ><input type="text"name ="txt10" /></td>
+</tr>
+
+<tr>
+<td colspan ="2" align="center"><input type="submit" value="submit"/></td>
+</tr>
+
+</table>
+
+<span/>
+
+<a href="https://twitter.com/Its_Coffee_Time" ">
+<img src="rj2.jpg" width="150"height ="50" ></a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</br>
+<b><a href="https://rahuljaincse.blogspot.com">Visit the Useful Tips Section</a></b>
+
+</body>
+</html>
